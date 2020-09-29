6aa413404442fa6317780ece5d0d2180115b8281
Larry N <larryn35@gmail.com>
2020-09-28 23:14:39 -0700

    initial commit

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Larry Nguyen on 9/28/20.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
